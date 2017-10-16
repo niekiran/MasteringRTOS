d8b63f16b62c93be3108c5a961c7c8bedc9f4bb8
Kiran <niekiran@gmail.com>
2017-10-16 12:03:05 +0530

    code changes and cleanup

--- Resources_RTOS/Source_codes/eclipse/sample_app/main.c
+++ Resources_RTOS/Source_codes/eclipse/sample_app/main.c
@@ -40,8 +40,6 @@
 #include "task.h" 
 #include "semphr.h"
 
-#include "stm32f4xx.h" 
-
 
 /* Used as a loop counter to create a very crude delay. */
 #define mainDELAY_LOOP_COUNT		( 0xfffff )
@@ -50,67 +48,12 @@
 void vTask1( void *pvParameters );
 void vTask2( void *pvParameters );
 
-extern void SystemInit(void);
-extern void SystemCoreClockUpdate(void);
-extern int stdout_init (void);
-
-/*-----------------------------------------------------------*/
-
-/*----------------------------------------------------------------------------
- * SystemCoreClockConfigure: configure SystemCoreClock using HSI
-                             (HSE is not populated on Nucleo board)
- *----------------------------------------------------------------------------*/
-void SystemCoreClockConfigure(void) {
-
-  RCC->CR |= ((uint32_t)RCC_CR_HSION);                     /* Enable HSI */
-  while ((RCC->CR & RCC_CR_HSIRDY) == 0);                  /* Wait for HSI Ready */
-
-  RCC->CFGR = RCC_CFGR_SW_HSI;                             /* HSI is system clock */
-  while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI);  /* Wait for HSI used as system clock */
-
-  FLASH->ACR  = (FLASH_ACR_PRFTEN     |                    /* Enable Prefetch Buffer */
-                 FLASH_ACR_ICEN       |                    /* Instruction cache enable */
-                 FLASH_ACR_DCEN       |                    /* Data cache enable */
-                 FLASH_ACR_LATENCY_5WS );                  /* Flash 5 wait state */
-
-  RCC->CFGR |= (RCC_CFGR_HPRE_DIV1  |                      /* HCLK = SYSCLK */
-                RCC_CFGR_PPRE1_DIV2 |                      /* APB1 = HCLK/2 */
-                RCC_CFGR_PPRE2_DIV1  );                    /* APB2 = HCLK/1 */
-
-  RCC->CR &= ~RCC_CR_PLLON;                                /* Disable PLL */
-
-  /* PLL configuration:  VCO = HSI/M * N,  Sysclk = VCO/P */
-  RCC->PLLCFGR = ( 16ul                   |                /* PLL_M =  16 */
-                 (200ul <<  6)            |                /* PLL_N = 200 */
-                 (  0ul << 16)            |                /* PLL_P =   2 */
-                 (RCC_PLLCFGR_PLLSRC_HSI) |                /* PLL_SRC = HSI */
-                 (  7ul << 24)            |                /* PLL_Q =   7 */
-                 (  2ul << 28)             );              /* PLL_R =   2 */
-
-  RCC->CR |= RCC_CR_PLLON;                                 /* Enable PLL */
-  while((RCC->CR & RCC_CR_PLLRDY) == 0) __NOP();           /* Wait till PLL is ready */
-
-  RCC->CFGR &= ~RCC_CFGR_SW;                               /* Select PLL as system clock source */
-  RCC->CFGR |=  RCC_CFGR_SW_PLL;
-  while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);  /* Wait till PLL is system clock src */
-}
-
 SemaphoreHandle_t xSemaphore = NULL;
 
 int main( void )
 {
-  
-	/* essential Board initializations */
-   SystemInit();
-	
-	 /* This function initializes the MCU clock, PLL will be used to generate Main MCU clock */
-	SystemCoreClockConfigure();  
-	SystemCoreClockUpdate();
-	
-	/* Trace init */
-	 
-	
-	trace_printf("Eclipse-FreeRTOS Project, Feb 11 , 2017 \n");
+
+	trace_printf("Eclipse-FreeRTOS Project Demo \n");
 	
 	
 	/* Create one of the two tasks. */
@@ -123,13 +66,7 @@ int main( void )
 
 	/* Create the other task in exactly the same way. */
 	xTaskCreate( vTask2, "Task 2", 240, NULL, 1, NULL );
-	
-	/* Create the other task in exactly the same way. */
-	xTaskCreate( vTask2, "crazy1", 240, NULL, 1, NULL );
-	
-	/* Create the other task in exactly the same way. */
-	xTaskCreate( vTask2, "crazy2", 240, NULL, 1, NULL );
-	
+    
   /* lets create the binary semaphore dynamically */
 	xSemaphore = xSemaphoreCreateBinary();
 	
