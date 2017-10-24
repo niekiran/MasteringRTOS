1f6473767cfc57004db26f71c0fb926b15f6661e
Kiran <niekiran@gmail.com>
2017-10-24 17:54:28 +0530

    Code modification blinky

--- Resources_RTOS/Source_codes/eclipse/Eclipse-ARM-Cortex-M/demo/blinky-STM32F4_NUCLEO/Debug/system/src/cmsis/subdir.mk
+++ Resources_RTOS/Source_codes/eclipse/Eclipse-ARM-Cortex-M/demo/blinky-STM32F4_NUCLEO/Debug/system/src/cmsis/subdir.mk
@@ -4,19 +4,30 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../system/src/cmsis/system_stm32f4xx.c \
-../system/src/cmsis/vectors_stm32f4xx.c 
+../system/src/cmsis/system_stm32f4xx.c 
+
+S_UPPER_SRCS += \
+../system/src/cmsis/startup_stm32f446xx.S 
 
 OBJS += \
-./system/src/cmsis/system_stm32f4xx.o \
-./system/src/cmsis/vectors_stm32f4xx.o 
+./system/src/cmsis/startup_stm32f446xx.o \
+./system/src/cmsis/system_stm32f4xx.o 
+
+S_UPPER_DEPS += \
+./system/src/cmsis/startup_stm32f446xx.d 
 
 C_DEPS += \
-./system/src/cmsis/system_stm32f4xx.d \
-./system/src/cmsis/vectors_stm32f4xx.d 
+./system/src/cmsis/system_stm32f4xx.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
+system/src/cmsis/%.o: ../system/src/cmsis/%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross Assembler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -x assembler-with-cpp -DDEBUG -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4xx" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 system/src/cmsis/%.o: ../system/src/cmsis/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM Cross C Compiler'
