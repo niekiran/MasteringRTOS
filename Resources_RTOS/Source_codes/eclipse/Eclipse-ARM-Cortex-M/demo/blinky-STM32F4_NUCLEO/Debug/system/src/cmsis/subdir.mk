5916e813accce11670dbde2b0e38da8e67e2ee74
Kiran <niekiran@gmail.com>
2017-10-24 17:41:57 +0530

    Eclipse setup for ARM Cortex MCU Added

---
+++ Resources_RTOS/Source_codes/eclipse/Eclipse-ARM-Cortex-M/demo/blinky-STM32F4_NUCLEO/Debug/system/src/cmsis/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../system/src/cmsis/system_stm32f4xx.c \
+../system/src/cmsis/vectors_stm32f4xx.c 
+
+OBJS += \
+./system/src/cmsis/system_stm32f4xx.o \
+./system/src/cmsis/vectors_stm32f4xx.o 
+
+C_DEPS += \
+./system/src/cmsis/system_stm32f4xx.d \
+./system/src/cmsis/vectors_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+system/src/cmsis/%.o: ../system/src/cmsis/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DSTM32F446xx -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4xx" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
